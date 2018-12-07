a1427f25bd60a9bcc107fd57bb58b27697c60cc1
Sullivan-County-Counter-Mapping <mappinghighlandny@gmail.com>
2018-12-06 21:02:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This isn't necessary but it keeps the editor from thinking L and carto are typos
+/* global L, carto */
 
 var map = L.map('map', {
         minZoom: 1,
@@ -6,23 +8,26 @@ var map = L.map('map', {
 
 map.setView([37, -96], 5);
 
+// Add base layer
 L.tileLayer('https://api.mapbox.com/styles/v1/data-maps/cjpcy434i397r2so8007sbne9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGF0YS1tYXBzIiwiYSI6ImNqZWpmeXhoajNtb2Eyd3FldG93OGpxejgifQ.xzOcuUd0LChdzHktllsAHw', { 
 }).addTo(map);
 
+// Initialize Carto
 var client = new carto.Client({
   apiKey: 'f18cab8055a91a04b4b9953a1f0c8fec11fc547c',
   username: 'mappinghighlandny'
 });
 
+// Initialze source data
 var source = new carto.source.SQL('SELECT * FROM eia_pipelines_dissolved_240');
 
+// Create style for the data
 var style = new carto.style.CartoCSS(`
  #eia_pipelines_dissolved_240{
   line-color: #0c2a2a;
   line-width: 1;
   line-opacity: 0.9;
 }
-`);
 `);
 
 // Add style to the data
@@ -32,12 +37,19 @@ var layer = new carto.layer.Layer(source, style);
 client.addLayer(layer);
 client.getLeafletLayer().addTo(map);
 
+/*
+ * Listen for changes on the layer picker
+ */
+
+// Step 1: Find the dropdown by class. If you are using a different class, change this.
 var layerPicker = document.querySelector('.layer-picker');
 
+// Step 2: Add an event listener to the dropdown. We will run some code whenever the dropdown changes.
 layerPicker.addEventListener('change', function (e) {
   // The value of the dropdown is in e.target.value when it changes
   var operator = e.target.value;
   
+  // Step 3: Decide on the SQL query to use and set it on the datasource
   if (operator === 'all') {
     // If the value is "all" then we show all of the features, unfiltered
     source.setQuery("SELECT * FROM eia_pipelines_dissolved_240");
@@ -47,5 +59,6 @@ layerPicker.addEventListener('change', function (e) {
     source.setQuery("SELECT * FROM eia_pipelines_dissolved_240 WHERE operator = '" + operator + "'");
   }
   
+  // Sometimes it helps to log messages, here we log the lifestage. You can see this if you open developer tools and look at the console.
   console.log('Dropdown changed to "' + operator + '"');
 });
